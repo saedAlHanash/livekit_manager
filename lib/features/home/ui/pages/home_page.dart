88f3d5d9de251daa2be7cafc7dccc464f9793a74
saedAlHanash <SaedAlHanash@gmail.com>
2026-02-01 16:05:19 +0300

    like android

--- lib/features/home/ui/pages/home_page.dart
+++ lib/features/home/ui/pages/home_page.dart
@@ -32,37 +32,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   RoomCubit get cubit => context.read<RoomCubit>();
 
-  final _tokenCtrl = TextEditingController(text: AppSharedPreference.getToken);
-
-  Future<void> getToken() async {
-    final r = await APIService().callApi(
-      url: 'Index/GetJoinToken',
-      type: ApiType.post,
-      body: {
-        "identity": "Admin",
-        "name": "Admin",
-        "videoGrants": {
-          "canPublish": false,
-          "canPublishData": true,
-          "canSubscribe": true,
-          "room": "s2",
-          "roomAdmin": false,
-          "roomCreate": true,
-          "roomJoin": true,
-          "roomList": true
-        },
-        "attributes": {
-          "type": LkUserType.manager.index.toString(),
-        }
-      },
-    );
-
-    setState(() {
-      _tokenCtrl.text = r.jsonBodyPure['token'];
-      AppSharedPreference.cashToken(_tokenCtrl.text);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<RoomCubit, RoomInitial>(
@@ -85,27 +54,13 @@ class _HomePageState extends State<HomePage> {
                         children: [
                           DrawableText(text: state.url),
                           20.0.verticalSpace,
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 25),
-                            child: MyTextFormWidget(
-                              label: 'Token',
-                              controller: _tokenCtrl,
-                              iconWidget: IconButton(
-                                onPressed: () {
-                                  getToken();
-                                },
-                                icon: ImageMultiType(url: Icons.generating_tokens),
-                              ),
-                            ),
-                          ),
-                          20.0.verticalSpace,
                           MyButton(
                             width: 1.0.sw,
                             loading: state.loading,
                             onTap: () {
                               cubit
-                                ..setUrl(wsLink)
-                                ..setToken(_tokenCtrl.text)
+                                ..setUrl(widget.link)
+                                ..setToken(widget.token)
                                 ..connect();
                             },
                             text: 'Join',
